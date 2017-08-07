72f5eaa9f610579c54113723f9df2fbf5d640073
Brian LeVay <brian_levay@yahoo.com>
2017-08-07 01:59:53 +0000

    added basic square mosaic capability

--- mosaic/_js/index.js
+++ mosaic/_js/index.js
@@ -6,7 +6,9 @@ var state_vals = {
     img: null,
     imgHeight: 0, imgWidth: 0,
     displayHeight: 0, displayWidth: 0,
-    rotation: 0.0
+    rotation: 0.0,
+    RGB_XYZ_white: {'Xref':100.0,'Yref':100.0,'Zref':100.0},
+    XYZ_Lab_white: {'Xref':95.047,'Yref':100.000,'Zref':108.883}
 };
 
 // Root function for handling the image loading //
@@ -116,5 +118,149 @@ function updateImage(direction, sizeChange) {
 // This will be the root function for executing the mosaic drawing //
 
 function generateMosaic() {
+    var generateBtn = document.getElementById('generateMosaic');
+    var largerBtn = document.getElementById('largerImg');
+    var smallerBtn = document.getElementById('smallerImg');
+    var rotateLeftBtn = document.getElementById('rotateLeft');
+    var rotateRightBtn = document.getElementById('rotateRight');
+    generateBtn.disabled = true;
+    largerBtn.disabled = true;
+    smallerBtn.disabled = true;
+    rotateLeftBtn.disabled = true;
+    rotateRightBtn.disabled = true;
+        
+    var canvas = document.getElementById('img_canvas');
+    var overlay = document.getElementById('overlay_canvas');
+    var ctxCanvas = canvas.getContext('2d');
+    var ctxOverlay = overlay.getContext('2d');
+    
+    var nTilesWidthUser = Number(document.getElementById("nTilesWidth").value);
+    if ((isNaN(nTilesWidthUser)==true) || (nTilesWidthUser < 1) || (nTilesWidthUser > 100)) {
+        alert("Not a valid number of tiles in X direction");
+    } else {
+        var nTilesWidth = nTilesWidthUser;
+        var tileWidth = Math.ceil(canvas.width / nTilesWidth);
+        var tileHeight = tileWidth;
+        var nTilesHeight = Math.ceil(canvas.height / tileHeight);
+        var tileMargin = 5;
+        
+        var pixelArray = [];
+        var aveRGB = [];
+        var tlX, tlY;
+        var rgbFillStr = "";
+        for (var i = 0; i < nTilesHeight; i++) {
+            for (var j = 0; j < nTilesWidth; j++) {
+                tlX = (j*tileWidth);
+                tlY = (i*tileHeight);
+                pixelArray = ctxCanvas.getImageData(tlX, tlY, tileWidth, tileHeight).data;
+                aveRGB = averageRGB(pixelArray);
+                rgbFillStr = "rgb(" + aveRGB['R'] + "," + aveRGB['G'] + "," + aveRGB['B'] + ")";
+                ctxOverlay.lineWidth = tileMargin;
+                ctxOverlay.strokeStyle = "rgb(50,50,50)";
+                ctxOverlay.fillStyle = rgbFillStr;
+                ctxOverlay.fillRect(tlX, tlY, tileWidth, tileHeight);
+                ctxOverlay.strokeRect(tlX, tlY, tileWidth, tileHeight);
+            }
+        }
+    }
+    generateBtn.disabled = false;
+    largerBtn.disabled = false;
+    smallerBtn.disabled = false;
+    rotateLeftBtn.disabled = false;
+    rotateRightBtn.disabled = false;
     return;
+}
+
+// This averages the RGB values over an entire spot //
+
+function averageRGB(pixelArray) {
+    var aveRGB = {'R':0,'G':0,'B':0,'X':0,'Y':0,'Z':0,'L*':0,'a*':0,'b*':0};
+    var n_pixels = Math.round(pixelArray.length/4);
+    var RGB, XYZ, Lab = [];
+    for (var k=0, len=pixelArray.length; k<len; k+=4) {
+        RGB = [pixelArray[k+0],pixelArray[k+1],pixelArray[k+2]];
+        XYZ = RGBtoXYZ(RGB);
+        Lab = XYZtoLab(XYZ);
+        aveRGB['L*'] += Lab[0];
+        aveRGB['a*'] += Lab[1];
+        aveRGB['b*'] += Lab[2];
+    }
+    aveRGB['L*'] = aveRGB['L*']/n_pixels;
+    aveRGB['a*'] = aveRGB['a*']/n_pixels;
+    aveRGB['b*'] = aveRGB['b*']/n_pixels;
+    var XYZ_ave = LabToXYZ([aveRGB['L*'],aveRGB['a*'],aveRGB['b*']]);
+    aveRGB['X'] = XYZ_ave[0];
+    aveRGB['Y'] = XYZ_ave[1];
+    aveRGB['Z'] = XYZ_ave[2];
+    var RGB_ave = XYZtoRGB(XYZ_ave);
+    aveRGB['R'] = RGB_ave[0];
+    aveRGB['G'] = RGB_ave[1];
+    aveRGB['B'] = RGB_ave[2];
+    return aveRGB;
+}
+
+// This converts the RGB value to CIE XYZ //
+
+function RGBtoXYZ([R,G,B]) {
+    var Rn = R/255;
+    var Gn = G/255;
+    var Bn = B/255;
+    var r = Rn > 0.04045 ? Math.pow((Rn + 0.055)/1.055, 2.4) : (Rn/12.92);
+    var g = Gn > 0.04045 ? Math.pow((Gn + 0.055)/1.055, 2.4) : (Gn/12.92);
+    var b = Bn > 0.04045 ? Math.pow((Bn + 0.055)/1.055, 2.4) : (Bn/12.92);
+    var xn = (r * 0.4124) + (g * 0.3576) + (b * 0.1805);
+    var yn = (r * 0.2126) + (g * 0.7152) + (b * 0.0722);
+    var zn = (r * 0.0193) + (g * 0.1192) + (b * 0.9505);
+    var X = xn * state_vals['RGB_XYZ_white']['Xref'];
+    var Y = yn * state_vals['RGB_XYZ_white']['Yref'];
+    var Z = zn * state_vals['RGB_XYZ_white']['Zref'];
+    return [X,Y,Z];
+}
+
+function XYZtoRGB([X,Y,Z]) {
+    var xn = X / state_vals['RGB_XYZ_white']['Xref'];
+    var yn = Y / state_vals['RGB_XYZ_white']['Yref'];
+    var zn = Z / state_vals['RGB_XYZ_white']['Zref'];
+    var r = (xn * 3.2406) + (yn * -1.5372) + (zn * -0.4986);
+    var g = (xn * -0.9689) + (yn * 1.8758) + (zn * 0.0415);
+    var b = (xn * 0.0557) + (yn * -0.2040) + (zn * 1.0570);
+    var Rn = r > 0.0031308 ? 1.055*Math.pow(r,(1.0/2.4)) - 0.055 : (r/12.92);
+    var Gn = g > 0.0031308 ? 1.055*Math.pow(g,(1.0/2.4)) - 0.055 : (g/12.92);
+    var Bn = b > 0.0031308 ? 1.055*Math.pow(b,(1.0/2.4)) - 0.055 : (b/12.92);
+    var R = Math.round(Rn * 255);
+    var G = Math.round(Gn * 255);
+    var B = Math.round(Bn * 255);
+    return [R,G,B];
+}
+
+// This converts the CIE XYZ to Lab //
+
+function XYZtoLab([X,Y,Z]) {
+    var xn = X / state_vals['XYZ_Lab_white']['Xref'];
+    var yn = Y / state_vals['XYZ_Lab_white']['Yref'];
+    var zn = Z / state_vals['XYZ_Lab_white']['Zref'];
+    var epsilon = 0.008856;
+    var kappa = 903.3;
+    var fx = xn > epsilon ? Math.pow(xn, (1.0/3.0)) : (kappa*xn + 16)/116;
+    var fy = yn > epsilon ? Math.pow(yn, (1.0/3.0)) : (kappa*yn + 16)/116;
+    var fz = zn > epsilon ? Math.pow(zn, (1.0/3.0)) : (kappa*zn + 16)/116;
+    var L = 116*fy - 16;
+    var a = 500*(fx - fy);
+    var b = 200*(fy - fz);
+    return [L,a,b];
+}
+
+function LabToXYZ([L,a,b]) {
+    var fy = (L + 16)/116;
+    var fx = a/500 + fy;
+    var fz = fy - (b/200);
+    var epsilon = 0.008856;
+    var kappa = 903.3;
+    var xn = Math.pow(fx,3) > epsilon ? Math.pow(fx,3) : (116*fx - 16)/kappa;
+    var yn = L > (kappa*epsilon) ? Math.pow(((L + 16)/116),3) : L/kappa;
+    var zn = Math.pow(fz,3) > epsilon ? Math.pow(fz,3) : (116*fz - 16)/kappa;
+    var X = xn * state_vals['XYZ_Lab_white']['Xref'];
+    var Y = yn * state_vals['XYZ_Lab_white']['Yref'];
+    var Z = zn * state_vals['XYZ_Lab_white']['Zref'];
+    return[X,Y,Z];
 }
